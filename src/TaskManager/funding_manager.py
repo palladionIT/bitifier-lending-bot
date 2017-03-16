aef89ad21e91492f39b24437f82beb0a266ee2c9
Matthias Gusenbauer <matthias.gusenbauer@palladion.it>
2017-03-16 18:36:09 +0100

    [REFACTOR] finished features - untested

--- src/TaskManager/funding_manager.py
+++ src/TaskManager/funding_manager.py
@@ -1,5 +1,7 @@
 import threading
 import time
+import math
+#import logging
 
 from configparser import ConfigParser
 from src.account import Account
@@ -11,30 +13,58 @@ class FundingManager(threading.Thread):
     DBLock = None
     API = None
     Accounts = []
+    Logger = None
 
+    # Execution Counter for clean tasks
     RunCounter = 0
 
-    def __init__(self, db_connector, db_lock, accounts, api):
+    # Account information
+    # [USD, BTC, ...]
+    balance = {'usd': 0, 'btc': 0}
+    limit = {'usd': 50, 'btc': -1}
+
+    # Account information
+    # [USD, BTC, ...]
+    exchange_rate = {'btc': 0}
+
+    # Account Margin Lend Parameters
+    # [USD, BTC, ...]
+    offers = {'usd': [], 'btc': []}
+    loan_time = {'usd': 2, 'btc': 2}
+    min_loan_spread = {'usd': 25000, 'btc': 25000}
+    max_loan_spread = {'usd': 100000, 'btc': 100000}
+    spread_cnt = {'usd': 5, 'btc': 3}
+    min_lend_rate = {'usd': 0.05, 'btc': 0.0175}
+    high_hold_amount = {'usd': 150, 'btc': 0.1}
+    high_hold_limit = {'usd': 0.15, 'btc': 0.1}  # Interest rate / year
+    high_hold_threshold = {'usd': 40, 'btc': 40}
+
+    def __init__(self, db_connector, db_lock, accounts, api, logger):
         # Todo: pass necessary arguments || create objects here
         self.DBConnector = db_connector
         self.DBLock = db_lock
         self.Accounts = accounts
         self.API = api
+        self.Logger = logger
 
     def run(self):
-        # Todo: refactor time loop code to be run here
-        # Todo: implement exception handling that restarts everything after upon next run
-        while True:
-            print('Running 10 minute task')
-            self.run_counter += 1
+        # Todo: maybe try-except inside while loop to locally contain errors
+        try:
+            while True:
+                print('Running 10 minute task')
+                self.run_counter += 1
 
-            if self.run_counter >= 6:
-                for account in self.Accounts:
-                    account.update_config(self.load_config(account.UserID))
-                    self.run_counter = 0
-            self.run_frequent_task()
-            print('Finished Running 10 minute task')
-            time.sleep(600)
+                if self.run_counter >= 6:
+                    for account in self.Accounts:
+                        account.update_config(self.load_config(account.UserID))
+                        self.run_counter = 0
+                self.run_frequent_task()
+                print('Finished Running 10 minute task')
+                time.sleep(600)
+        except Exception as e:
+            #self.Logger.error('FUNDING MANAGER ')
+            print('FUNDING MANAGER: Error while handling recurring task.')
+            print(e)
 
     def run_frequent_task(self):
         for account in self.Accounts:
@@ -43,23 +73,24 @@ class FundingManager(threading.Thread):
                 account.api_test()
                 account.offer_funding()
 
-    # TODO: remove this debugging function
-    def arch_change(self):
-        for acc in self.Accounts:
-            # self.API.check_authentication(acc)
-            success, code, response = self.API.get_ticker({'symbol': 'USD'})
-
-            # TODO: CONTINUE HERE AFTER AWS OUTAGE - TEST ALL API CALLS && UPDATE METHODS
-
-            if success != True:
-                print('shit!')
-
     def offer_funding(self):
         self.clear_active_offers()
         self.get_available_funds()
         self.load_exchange_rates()
-        self.calculate_limits()
+        self.calculate_limts()
         offers = self.calculate_offers()
+
+        for currency, loans in offers.items():  # Todo: write information to database
+            for loan in loans:
+                success, return_code, response = self.API.funding_new_offer(currency=currency,
+                                                                            amount=loan['amt'],
+                                                                            rate=loan['rate'],
+                                                                            period=loan['time'],
+                                                                            direction='lend')
+
+                if not success:
+                    print('Error ' + return_code + ' creating funding lends.')
+                    print (str(response))
 
 
     def clear_active_offers(self,):
@@ -71,10 +102,15 @@ class FundingManager(threading.Thread):
             if not success:
                 print('Error ' + return_code + ' cancelling active funding lends.')
                 print(str(response))
-        pass
 
     def get_active_offers(self):
-        pass
+        success, return_code, response = self.API.funding_active_offer()
+
+        if not success:
+            print('Error ' + return_code + ' retrieving active funding lends.')
+            print(str(response))
+        else:
+            return response
 
     def get_taken_offers(self):
         pass
@@ -83,19 +119,104 @@ class FundingManager(threading.Thread):
         pass
 
     def get_available_funds(self):
-        pass
+        success, return_code, response = self.API.get_wallet_balance()
+
+        if success:
+            for info in response:
+                if info['type'] == 'deposit':
+                    self.balance[info['currency']] = float(info['available'])
 
     def calculate_offers(self):
-        pass
+        amount_asks = 1000
+        amount_bids = 0
+        offers = {}
+
+        for currency, balance in self.balance.items():
+            loans = []
+            if balance >= self.limit[currency]:
+                cnt = 1
+                available = balance
+
+                fundbook = self.API.get_fundingbook(currency=currency,
+                                                    limit_asks=str(amount_asks),
+                                                    limit_bids=str(amount_bids))[2]
+
+                # Calculate if lendbook is even in the neighbourhood of our desired high lend position
+                do_high_lend = True
+                lendbook_aggregate = 0
+                for book_entry in fundbook['asks']:
+                    lendbook_aggregate += float(book_entry['amount'])
+                    if float(book_entry['rate']) >= self.high_hold_limit[currency] * 365:
+                        # Todo more sophisticated distance calculation
+                        if lendbook_aggregate <= 3 * self.max_loan_spread[currency]:
+                            pass
+                        else:
+                            do_high_lend = False
+
+                if self.high_hold_amount[currency] > self.limit[currency] and do_high_lend:
+                    # If we have enough funds place order
+                    if available > self.high_hold_amount[currency]:
+                        available -= self.high_hold_amount[currency]
+
+                        loans.append({'amt': str(balance if self.high_hold_amount[currency] > balance else self.high_hold_amount[currency]),
+                                      'rate': str(self.high_hold_limit[currency] * 365),
+                                      'time': 30
+                                      })
+                    # Else reserve the money and wait
+                    else:
+                        available -= self.high_hold_amount[currency]
+
+                if available > self.limit[currency]:
+
+                    split_cnt = self.spread_cnt[currency]
+                    split_amount = math.floor((available / split_cnt) * 100) / 100
+                    while split_amount < self.limit[currency]:  # Todo: also implement min value split behaviour
+                        split_cnt -= 1
+                        if split_cnt > 0:
+                            split_amount = math.floor((available / split_cnt) * 100) / 100
+                        else:
+                            split_cnt = 1
+                            split_amount = available
+                            break
+
+                    if split_cnt >= 1:
+                        lendbook_aggregate = 0
+                        split_climb = ((self.max_loan_spread[currency] - self.min_loan_spread[currency]) / (split_cnt - 1 )) if (split_cnt -1 ) > 0 else 0
+                        next_loan = self.min_loan_spread[currency]
+
+                        min_rate_annual = self.min_lend_rate[currency] * 365
+
+                        for book_entry in fundbook['asks']:
+                            lendbook_aggregate += float(book_entry['amount'])
+                            while lendbook_aggregate >= next_loan and cnt <= split_cnt:
+                                loans.append({'amt': str(split_amount),
+                                              'rate': str(float(book_entry['rate']) - 0.0001 if (float(book_entry['rate']) - 0.0001) > min_rate_annual else min_rate_annual),
+                                              'time': 30 if self.high_hold_threshold[currency] > 0 and float(book_entry['rate']) > self.high_hold_threshold[currency] * 365 else 2
+                                              })
+                                next_loan += split_climb
+                                cnt += 1
+
+                            if cnt > split_cnt:
+                                break
+            offers[currency] = loans
+        return offers
 
     def usd_from_btc(self, btc):
-        pass
+        btc_price = self.API.get_ticker('btcusd')[2]
+        return btc * float(btc_price['last_price'])
 
     def load_exchange_rates(self):
-        pass
+        symbols = self.API.get_symbols()[2]
+        for symbol in symbols:
+            if symbol[3:] == 'usd':
+                symbol_info = self.API.get_ticker(symbol=symbol)[2]
+                self.exchange_rate[symbol[:3]] = float(symbol_info['last_price'])
 
-    def calculate_limits(self):
-        pass
+    def calculate_limts(self):
+        for currency, rate in self.exchange_rate.items():
+            self.limit[currency] = self.limit['usd'] / rate
+            self.min_loan_spread[currency] = self.min_loan_spread['usd'] / rate
+            self.max_loan_spread[currency] = self.max_loan_spread['usd'] / rate
 
     @staticmethod
     def load_config(acc_id):
