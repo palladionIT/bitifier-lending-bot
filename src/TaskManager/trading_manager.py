4876567fcf3274ce784b3233ff92858a19a455a7
Matthias Gusenbauer <matthias.gusenbauer@palladion.it>
2017-03-18 21:37:58 +0100

    [FEATURE] market data retrieval and moving average computation(smoother)

--- src/TaskManager/trading_manager.py
+++ src/TaskManager/trading_manager.py
@@ -1,6 +1,7 @@
 import threading
 import time
 import logging
+import numpy as np
 
 from configparser import ConfigParser
 from src.account import Account
@@ -51,20 +52,49 @@ class TradingManager(threading.Thread):
         '''
 
     def check_market_data(self):
-        # Todo: get current market data
-        #
         trading_pair = 'XXBTZEUR'
+        interval = '1' # in minutes
 
         market_state = self.API.query_public('OHLC', {'pair': trading_pair,
-                                                      'interval': '15'})
+                                                      'interval': interval})
 
         if len(market_state['error']) == 0:
             market_data = market_state['result'][trading_pair]
+
+            # Data extraction of compound list
+            current_period = market_data[-1]
+            clean_data = market_data[:-1]
+            times = [sublist[0] for sublist in clean_data]
+            vw_average = [sublist[5] for sublist in clean_data]
+
+            smooth_vw_average = self.smooth_data([int(i) for i in times], [float(i) for i in vw_average], 5, 'moving_average')
             # Todo: handle this data - compute minima/maxima
             # Todo: fit curve to market data
             # Todo: calculate maxima/minima && trend
             # Todo: return something (buy? sell?)
+            # methods:
+            # https://stackoverflow.com/questions/7061071/finding-the-min-max-of-a-stock-chart
+            # https://en.wikipedia.org/wiki/Moving_average#Other_weightings
+            # en.wikipedia.org/wiki/Local_regression
+            # https://en.wikipedia.org/wiki/Kernel_smoother
+            # https://en.wikipedia.org/wiki/Moving_least_squares
         pass
+
+    def smooth_data(self, x_dat, y_dat, interval, type):
+        smoothed_data = None
+        if type == 'moving_average':
+            # Adds border extension to avoid artifacts for convolution
+            # Gets size of window for given timeframe
+            # Convolves and trims padding elements
+            interval_s = interval * 60
+            x_ind = np.argmax(np.asarray([x - x_dat[0] for x in x_dat]) >= interval_s)
+            window = np.ones(int(x_ind))/float(x_ind)
+            padding_start = [y_dat[0]] * x_ind # To extend at the border to remove border error
+            padding_end = [y_dat[-1]] * x_ind
+            smoothed_data = np.convolve(padding_start+y_dat+padding_end, window, 'same')[x_ind:-x_ind]
+
+        return smoothed_data
+
 
     @staticmethod
     def load_config(acc_id):
